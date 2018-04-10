3b32578d69f097bc8d23d740c3c9de9764dbe5f3
Kenerry Pierry Paulo Serain <kserain.development@gmail.com>
2018-04-10 20:34:17 -0300

    First

---
+++ source/repos/MoneyTime/MoneyTime/IdentityConfig/IdentityConfiguration.cs
@@ -0,0 +1,48 @@
+﻿using IdentityServer4.Models;
+using MoneyTime.IdentityModel;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MoneyTime.IdentityConfig
+{
+    public class IdentityConfiguration
+    {
+        public static IEnumerable<ApiResource> GetApiResources(IdentitySettings identitySettings)
+        {
+            var apiResources = new List<ApiResource>();
+            foreach (var item in identitySettings.ApiResources ?? Enumerable.Empty<IdentityApiResource>())
+            {
+                apiResources.Add(new ApiResource
+                {
+                    Name = item.Name,
+                    DisplayName = item.DisplayName
+                });
+            }
+
+            return apiResources;
+        }
+
+        public static IEnumerable<Client> GetClients(IdentitySettings identitySettings)
+        {
+            var clients = new List<Client>();
+            foreach (var item in identitySettings.Clients ?? Enumerable.Empty<IdentityClient>())
+            {
+                clients.Add(new Client
+                {
+                    ClientId = item.ClientId,
+                    ClientName = item.ClientName,
+                    ClientUri = item.ClientUri,
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret(item.Secret.Sha256())
+                    },
+                    AllowedScopes = item.AllowedScopes,
+                    AccessTokenLifetime = item.AccessTokenLifetime
+                });
+            }
+
+            return clients;
+        }
+    }
+}
---
+++ source/repos/MoneyTime/src/MoneyTime/IdentityModel/IdentitySettings.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace MoneyTime.IdentityModel
+{
+    public class IdentitySettings
+    {
+        public string IdentityEndPoint { get; set; }
+        public IEnumerable<IdentityClient> Clients { get; set; }
+        public IEnumerable<IdentityApiResource> ApiResources { get; set; }
+    }
+}
