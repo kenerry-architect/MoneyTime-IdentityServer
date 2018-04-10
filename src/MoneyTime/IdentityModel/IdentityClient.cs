992956c01fec748795744640cfd45bbafcf6910f
kenerry-architect <38140596+kenerry-architect@users.noreply.github.com>
2018-04-10 20:48:23 -0300

    Add files via upload

---
+++ src/MoneyTime/IdentityModel/IdentityClient.cs
@@ -0,0 +1,12 @@
+﻿namespace MoneyTime.IdentityModel
+{
+    public class IdentityClient
+    {
+        public string ClientId { get; set; }
+        public string ClientName { get; set; }
+        public string ClientUri { get; set; }
+        public string Secret { get; set; }
+        public string[] AllowedScopes { get; set; }
+        public int AccessTokenLifetime { get; set; }
+    }
+}
---
+++ src/MoneyTime/Startup.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MoneyTime.IdentityConfig;
+using MoneyTime.IdentityModel;
+
+namespace MoneyTime
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<IdentitySettings>(Configuration);
+            var settings = Configuration.Get<IdentitySettings>();
+            services.AddSingleton(settings);
+
+            services.AddIdentityServer
+            (
+                options =>
+                {
+                    options.IssuerUri = settings.IdentityEndPoint;
+                    options.Endpoints.EnableDiscoveryEndpoint = true;
+                    options.Endpoints.EnableTokenEndpoint = true;
+                    options.Endpoints.EnableUserInfoEndpoint = true;
+
+                    options.Endpoints.EnableAuthorizeEndpoint = false;
+                    options.Endpoints.EnableCheckSessionEndpoint = false;
+                    options.Endpoints.EnableEndSessionEndpoint = false;
+                    options.Endpoints.EnableIntrospectionEndpoint = false;
+                    options.Endpoints.EnableTokenRevocationEndpoint = false;
+                }
+            )
+            .AddDeveloperSigningCredential()
+            .AddInMemoryApiResources(IdentityConfiguration.GetApiResources(settings))
+            .AddInMemoryClients(IdentityConfiguration.GetClients(settings));
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
+
+            app.UseIdentityServer();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
